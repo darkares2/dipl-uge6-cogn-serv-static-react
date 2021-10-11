bd8408a9a54e852f26254cd0f27bbe10dde54be6
rasputin <rasputin@darkares.dk>
2021-10-11 17:16:48 +0200

    Try to change omehting

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello, the {value} is not enough</div>;
 }
 
 export default App;
