d0858a5766ff18f3ecf1a2f0a3fd9319aab5e8f3
rasputin <rasputin@darkares.dk>
2021-10-11 17:33:33 +0200

    Initial url setup wiht button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
+import validator from 'validator'
+  
+const App = () => {
+  
+  const [errorMessage, setErrorMessage] = useState('')
+  const [isDisabled, setDisabled] = useState(true);
 
-function App() {
-  const value = 'World';
-  return <div>Hello, the {value} is not enough</div>;
+  const validate = (value) => {
+    
+    if (validator.isURL(value)) {
+      setErrorMessage('Is Valid URL')
+      setDisabled(false);
+    } else {
+      setErrorMessage('Is Not Valid URL')
+      setDisabled(true);
+    }
+  }
+
+  const analyzeImage = () => {
+    
+  }
+  
+  return (<div>
+    <h2>Enter image url to analyze</h2>
+        <span>Enter URL: </span><input type="text" 
+        onChange={(e) => validate(e.target.value)}></input> <br />
+        <span style={{
+          fontWeight: 'bold',
+          color: 'red',
+        }}>{errorMessage}</span>
+        <br />
+        <button onClick={analyzeImage} disabled={isDisabled}>
+          Analyze image!
+        </button>
+  </div>);
 }
 
 export default App;
